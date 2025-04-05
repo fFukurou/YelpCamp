3211b848448b39bd04528aef63d60f52a0bb0588
fFukurou <supevi1@gmail.com>
2025-04-04 23:47:47 -0300

    added bootstrap styling

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
+// MONGOOSE CONNECTION 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
-
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -17,6 +18,7 @@ const app = express();
 
 // MIDDLEWARE
 app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
--- seeds/index.js
+++ seeds/index.js
@@ -18,10 +18,14 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
 
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officia, laboriosam rem necessitatibus autem accusamus nihil nulla delectus, eligendi aspernatur, sunt iste debitis ab velit! Libero voluptatem id facere! Fuga?',
+            price: price
         })
         await camp.save();
     }
